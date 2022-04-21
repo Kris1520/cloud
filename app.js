fc17ea35d1a24ec5c5b30cfd0eed1b32b289899a
Student User <student@workstation.lab.example.com>
2022-04-21 16:46:13 -0400

    commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//console.log("Hello World!\n");
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+      res.send('Hello, Worldr!\n');
+});
+
+app.listen(8080, function () {
+      console.log('Example app listening on port 8080!');
+});
+
+module.exports = app;
